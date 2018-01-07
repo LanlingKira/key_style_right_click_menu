86f5aff31304bf73a54328b3a2afc0fb3a3d0659
LanlingKira <lanlingkira@163.com>
2018-01-07 18:04:06 +0800

    Update and bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,61 @@
-# key_style_right_click_menu
-仿Key游戏风格jQuery右键菜单
-## 示例地址：
-[http://thkira.com/key.html](http://thkira.com/key.html "仿Key游戏风格jQuery右键菜单") 
+# GalMenu.js 右键环形菜单
+## 使用方法
+### 引入文件
+在页面中引入 GalMenu.css，jquery 和 GalMenu.js 文件
+``` html
+<link rel="stylesheet" type="text/css" href="css/GalMenu.css">
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/GalMenu.js"></script>
+```
+### HTML 结构
+为菜单项设置相应的跳转或点击事件
+``` html
+<div class="galMenu galRing">
+  <div class="circle" id="gal">
+    <div class="ring">
+      <a href="#" title="Home" class="menuItem">Home</a>
+      <a href="#" title="Blog" class="menuItem">Blog</a>
+      <a href="#" title="About" class="menuItem">About</a>
+      <a href="#" title="Contact" class="menuItem">Contact</a>
+      <a href="#" title="Social" class="menuItem">Social</a>
+      <a href="#" title="Other" class="menuItem">Other</a>
+    </div>
+    <audio id="galAudio" src="audio/nyanpass.mp3"></audio>
+  </div>
+</div>
+<div id="overlay" style="opacity: 1; cursor: pointer;"></div>
+```
+### CSS 样式
+为右键菜单的菜单项设置背景图片
+``` css
+.ring a:nth-of-type(1){background-image:url(1.jpg)}
+.ring a:nth-of-type(2){background-image:url(2.jpg)}
+.ring a:nth-of-type(3){background-image:url(3.jpg)}
+.ring a:nth-of-type(4){background-image:url(4.jpg)}
+.ring a:nth-of-type(5){background-image:url(5.jpg)}
+.ring a:nth-of-type(6){background-image:url(6.jpg)}
+```
+### 初始化插件
+在页面 DOM 元素加载完毕之后，通过 galMenu() 方法来初始化该右键菜单插件
+``` javascript
+$(document).ready(function() {
+  $("body").galMenu({
+    menu:"galRing",
+    click_to_close:true,
+    stay_open:false
+  });
+});
+```
+### 配置参数
+GalMenu.js 有 2 个可用的配置参数
+ - `click_to_close`：是否在点击遮罩层时关闭右键菜单，默认为 true
+ - `stay_open`：是否在点击菜单项后一直显示右键菜单，默认为 false
 
+### 关闭菜单
+3 个方式
+ - 再次右键
+ - 左键点击遮罩层
+ - ESC 关闭
+
+## 演示地址：
+[https://thkira.com/key.html](http://thkira.com/key.html "仿Key游戏风格jQuery右键菜单") 
--- js/GalMenu.js
+++ js/GalMenu.js
@@ -1,119 +1,138 @@
-; (function($) {
-    var GalMenu = {
-        defaults: {
-            click_to_close: true,
-            stay_open: false
+(function($) {
+    var galMenu = {
+        defaults:{
+            menu:"galRing",
+            click_to_close:true,
+            stay_open:false
         },
-        init: function(options) {
-            var o = options,
-            $this = $(this);
+        init:function(options) {
+            var o = options, audio = $("#galAudio")[0], $this = $(this);
             $this.each(function(i) {
-                var $this = $(this),
-                settings = $.extend({},
-                GalMenu.defaults, o),
-                $menu = $('.' + settings.menu);
-                $this.on('mousedown',
-                function(e) {
-                    if (e.which !== 3 && $(e.target).parents('.GalMenu').length < 1 && settings.click_to_close) {
-                        $this.find('.GalMenu').stop(true, false).animate({
-                            opacity: 0
-                        },
-                        {
-                            duration: 100,
-                            queue: false,
-                            complete: function() {
-                                $(this).css('display', 'none').find('.active').removeClass('active').next().stop(true, true).slideUp('normal')
-                            }
-                        });
-                        $(".circle").removeClass("open");
-                        $("#overlay").hide()
+                var $this = $(this), settings = $.extend({}, galMenu.defaults, o), $menu = $("." + settings.menu);
+                function galOpen(e) {
+                    galMenu.getCoords(e);
+                    var top = Coords.clientY - 150, left = $("body")[0] === e.target ? Coords.clickX - 150 :Coords.clientX - 150;
+                    var bodyHe = document.documentElement.clientHeight;
+                    var bodyWi = document.documentElement.clientWidth;
+                    if (top < 0) {
+                        top = 0;
+                    }
+                    if (bodyHe - Coords.clientY < 150) {
+                        top = bodyHe - 300;
+                    }
+                    if (left < 0) {
+                        left = 0;
+                    }
+                    if ($("body")[0] === e.target) {
+                        if (bodyWi - Coords.clickX < 150) {
+                            left = bodyWi - 300;
+                        }
+                    } else {
+                        if (bodyWi - Coords.clientX < 150) {
+                            left = bodyWi - 300;
+                        }
+                    }
+                    $menu.css({
+                        top:top + "px",
+                        left:left + "px",
+                        display:"block"
+                    }).stop(true, false).animate({
+                        opacity:1
+                    }, {
+                        duration:100,
+                        queue:false
+                    });
+                    $(".circle").addClass("open");
+                    $("#overlay,.galMenu").show();
+                    audio.play();
+                }
+                function galClose(e) {
+                    $(".circle").removeClass("open");
+                    $("#overlay").hide();
+                    $(".galMenu").delay(400).hide(0);
+                    audio.pause();
+                    audio.currentTime = 0;
+                }
+                if (!settings.stay_open) {
+                    var $a = $this.find("div.ring > a");
+                    $a.on("click", function(e) {
+                        galClose(e);
+                    });
+                }
+                $this.on("mousedown", function(e) {
+                    if (e.which !== 3 && $(e.target).parents(".galMenu").length < 1 && settings.click_to_close) {
+                        galClose(e);
                     }
                 });
-                $this.on('contextmenu',
-                function(e) {
+                $this.on("contextmenu", function(e) {
                     e.preventDefault();
                     e.stopPropagation();
-                    GalMenu.getCoords(e);
-                    $('.GalMenu_close_me').stop(true, false).animate({
-                        opacity: 0
-                    },
-                    {
-                        duration: 100,
-                        queue: false,
-                        complete: function() {
-                            $(this).css('display', 'none')
-                        }
-                    });
-                    var audio = $("#audio")[0];
-                    var add = 150;
-                    var top = Coords.clientY - add,
-                    left = ($('body')[0] === e.target) ? Coords.clickX + add: Coords.clientX - add;
-                    $menu.css({
-                        top: top + 'px',
-                        left: left + 'px',
-                        display: 'block'
-                    }).stop(true, false).animate({
-                        opacity: 1
-                    },
-                    {
-                        duration: 100,
-                        queue: false
-                    });
                     if ($("#gal").hasClass("open")) {
-                        $(".circle").removeClass("open");
-                        $("#overlay").hide();
-                        audio.pause();
-                        audio.currentTime = 0
+                        galClose(e);
                     } else {
-                        $(".circle").addClass("open");
-                        $("#overlay").show();
-                        audio.play()
+                        galOpen(e);
                     }
-                })
-            })
+                });
+                $this.keyup(function(e) {
+                    if (e.keyCode == 27) {
+                        if ($("#gal").hasClass("open")) {
+                            galClose(e);
+                        }
+                    }
+                });
+            });
         },
-        getCoords: function(e) {
-            var evt = e ? e: window.event;
-            var clickX = 0,
-            clickY = 0;
+        getCoords:function(e) {
+            var evt = e ? e :window.event;
+            var clickX = 0, clickY = 0;
             if ((evt.clientX || evt.clientY) && document.body && document.body.scrollLeft != null) {
                 clickX = evt.clientX + document.body.scrollLeft;
-                clickY = evt.clientY + document.body.scrollTop
-            };
-            if ((evt.clientX || evt.clientY) && document.compatMode == 'CSS1Compat' && document.documentElement && document.documentElement.scrollLeft != null) {
+                clickY = evt.clientY + document.body.scrollTop;
+            }
+            if ((evt.clientX || evt.clientY) && document.compatMode == "CSS1Compat" && document.documentElement && document.documentElement.scrollLeft != null) {
                 clickX = evt.clientX + document.documentElement.scrollLeft;
-                clickY = evt.clientY + document.documentElement.scrollTop
-            };
+                clickY = evt.clientY + document.documentElement.scrollTop;
+            }
             if (evt.pageX || evt.pageY) {
                 clickX = evt.pageX;
-                clickY = evt.pageY
-            };
-            return Coords = {
-                clickX: clickX,
-                clickY: clickY,
-                clientX: evt.clientX,
-                clientY: evt.clientY,
-                screenX: evt.screenX,
-                screenY: evt.screenY
+                clickY = evt.pageY;
             }
+            return Coords = {
+                clickX:clickX,
+                clickY:clickY,
+                clientX:evt.clientX,
+                clientY:evt.clientY,
+                screenX:evt.screenX,
+                screenY:evt.screenY
+            };
         }
     };
-    $.fn.GalMenu = function(method, options) {
-        if (GalMenu[method]) {
-            return GalMenu[method].apply(this, Array.prototype.slice.call(arguments, 1))
-        } else if (typeof method === 'object' || !method) {
-            return GalMenu.init.apply(this, arguments)
+    $.fn.galMenu = function(method, options) {
+        if (galMenu[method]) {
+            return galMenu[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === "object" || !method) {
+            return galMenu.init.apply(this, arguments);
         } else {
-            $.error('Method ' + method + ' does not exist')
+            $.error("Method " + method + " does not exist");
         }
-    }
+    };
+    $(document).ready(function() {
+        var items = document.querySelectorAll(".menuItem");
+        for (var i = 0, l = items.length; i < l; i++) {
+            items[i].style.left = (50 - 35 * Math.cos(-.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+            items[i].style.top = (50 + 35 * Math.sin(-.5 * Math.PI - 2 * (1 / l) * i * Math.PI)).toFixed(4) + "%";
+        }
+    });
 })(jQuery);
+
 String.prototype.removeWS = function() {
-    return this.toString().replace(/\s/g, '')
+    return this.toString().replace(/\s/g, "");
 };
+
 String.prototype.pF = function() {
-    return parseFloat(this)
+    return parseFloat(this);
 };
+
 Number.prototype.pF = function() {
-    return parseFloat(this)
+    return parseFloat(this);
 };
